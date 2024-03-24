2fb29494fb86bce0c06ec2001dd3a747d2ae2d0e
Katerina <katerina--777@mail.ru>
2024-03-24 18:58:29 +0700

    Add cls Vehicle with func start and move

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,24 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+        self.started = False
+
+    def start(self):
+        while not self.started:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise LowFuelError('Fuel is not enough to start')
+
+    def move(self, distance):
+        if self.fuel >= self.fuel_consumption * distance:
+            self.fuel -= (self.fuel_consumption * distance)
+        else:
+            raise NotEnoughFuel('Fuel is not enough')
+
